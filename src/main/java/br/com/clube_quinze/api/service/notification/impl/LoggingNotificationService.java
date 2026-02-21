fa0729af1ef094448fd754c0b763cad262d64e97
Andre0Luis <aluis283@gmail.com>
2026-02-21 00:10:02 -0300

    fix: mail 2

--- src/main/java/br/com/clube_quinze/api/service/notification/impl/LoggingNotificationService.java
+++ src/main/java/br/com/clube_quinze/api/service/notification/impl/LoggingNotificationService.java
@@ -3,10 +3,12 @@ package br.com.clube_quinze.api.service.notification.impl;
 import br.com.clube_quinze.api.service.notification.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(prefix = "app.notifications.logging", name = "enabled", havingValue = "true")
 public class LoggingNotificationService implements NotificationService {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingNotificationService.class);
